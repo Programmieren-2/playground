bc6ccf107cd1ef4668c58088f9d8ef7250a58471
Richard Neumann <mail@richard-neumann.de>
2021-04-15 00:03:06 +0200

    Add more class instances

--- src/main.cpp
+++ src/main.cpp
@@ -12,11 +12,13 @@ using std::vector;
 int main() {
 	Person peter("Peter", "Lustig");
 	Person otto("Otto", "Waalkes");
+	Person julie("Julie", "Fowlis");
 	Tweeter tweety("Tweety", "McPeep", "@tmp");
 	Tweeter djt("Donald", "John", "Trump", "@realdonaldtrump");
+	Tweeter katy("Katy", "Perry", "@katyperry");
 
-	vector<Person> persons = {peter, otto};
-	vector<Tweeter> tweeters = {tweety, djt};
+	vector<Person> persons = {peter, otto, julie};
+	vector<Tweeter> tweeters = {tweety, djt, katy};
 
 	printall<Person>(persons);
 	printall<Tweeter>(tweeters);
