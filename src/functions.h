b00443804abe2215d10b5954e7866a7f2ad8eb9f
Richard Neumann <mail@richard-neumann.de>
2021-04-15 17:29:11 +0200

    Remove space between function name and parameter list

--- src/functions.h
+++ src/functions.h
@@ -5,7 +5,7 @@
 #include <vector>
 
 template <class Streamable>
-void printall (std::vector<Streamable> items, std::string sep = "\n")
+void printall(std::vector<Streamable> items, std::string sep = "\n")
 {
 	for (Streamable item : items)
 		std::cout << item << sep;
