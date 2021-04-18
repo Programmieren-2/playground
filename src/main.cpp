f2de89f7407ec2f40a76ef35c41ba4f7bee6960c
Richard Neumann <mail@richard-neumann.de>
2021-04-19 01:42:01 +0200

    Add musicians

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 
 #include "functions.h"
 #include "person.h"
+#include "musician.h"
 #include "tweeter.h"
 
 using std::thread;
@@ -10,26 +11,34 @@ using std::vector;
 using prog2::getSorter;
 using prog2::printall;
 using prog2::Person;
+using prog2::Musician;
 using prog2::Tweeter;
 
 int main() {
-	Person peter("Peter", "Lustig");
-	Person otto("Otto", "Waalkes");
-	Person julie("Julie", "Fowlis");
-	Tweeter tweety("Tweety", "McPeep", "@tmp");
+	Person pl("Peter", "Lustig");
+	Person ow("Otto", "Waalkes");
+	Person ae("Anke", "Engelke");
+	Musician jf("Julie", "Fowlis", "vocals");
+	Musician kg("Kinga", "Głyk", "bass");
+	Musician ar("Arthur", "Rubinstein", "piano");
+	Tweeter tmp("Tweety", "McPeep", "@tmp");
 	Tweeter djt("Donald", "John", "Trump", "@realdonaldtrump");
-	Tweeter katy("Katy", "Perry", "@katyperry");
+	Tweeter kp("Katy", "Perry", "@katyperry");
 
-	vector persons = {peter, otto, julie};
-	vector tweeters = {tweety, djt, katy};
+	vector persons = {pl, ow, ae};
+	vector musicians = {jf, kg, ar};
+	vector tweeters = {tmp, djt, kp};
 
 	thread sortPersons(getSorter(persons));
+	thread sortMusicians(getSorter(musicians));
 	thread sortTweeters(getSorter(tweeters));
 
 	sortPersons.join();
+	sortMusicians.join();
 	sortTweeters.join();
 
 	printall(persons);
+	printall(musicians);
 	printall(tweeters);
 
 	return 0;
